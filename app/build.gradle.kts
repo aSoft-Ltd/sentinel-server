3e27cef51889efe94a6f27e2871074a129d40b30
andylamax <andylamax@programmer.net>
2023-10-12 09:56:22 +0300

    battling with mongo and docker

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,5 @@
-import com.android.build.gradle.internal.tasks.factory.dependsOn
 import docker.DockateExtension
 import docker.DockatePlugin
-import org.jetbrains.kotlin.gradle.targets.jvm.tasks.KotlinJvmTest
 
 plugins {
     kotlin("jvm")
@@ -25,53 +23,42 @@ kotlin {
             dependencies {
                 implementation(ktor.server.cio)
                 implementation(ktor.server.cors)
-                implementation(projects.sentinelRegistrationServiceSdk)
+                // implementation(projects.sentinelRegistrationServiceSdk)
             }
         }
 
         val test by getting {
             dependencies {
-                implementation(libs.kommander.coroutines)
+                // implementation(libs.kommander.coroutines)
             }
         }
     }
 }
 
 configure<DockateExtension> {
-    val network = createNetwork("sentinel")
+    addDockerImageTasksForJvmApp(name = "app", port = 8080)
 
-    val (runMongo, _, removeMongo) = addDockerContainerTasksForMongo(
-        image = "mongodb/mongodb-community-server:7.0.0-ubuntu2204",
-        network = network,
-        username = "root",
-        password = "pass",
-        port = 27017
-    )
+    dockerCompose {
+        version(3.8)
+        services {
+            val mongo = service("mongo") {
+//                image("mongodb/mongodb-community-server:7.0.0-ubuntu2204")
+                image("mongo:latest")
+                restart("always")
+                port(27017, 27017)
+                environment(
+                    "MONGODB_INITDB_ROOT_USERNAME" to "root",
+                    "MONGODB_INITDB_ROOT_PASSWORD" to "pass"
+                )
+                volumes("./data/db" to "/data/db")
+            }
 
-    val (createDockerfile, buildAppImage, removeAppImage) = addDockerImageTasksForJvmApp(
-        name = "app", port = 8080, directory = layout.buildDirectory.dir("install/$name")
-    )
-
-    val (runAppContainer, _, removeAppContainer) = addDockerContainerTasksFor(
-        name = "app",
-        image = "app",
-        network = network,
-        args = arrayOf("-p", "8080:8080")
-    )
-
-    createDockerfile.configure {
-        dependsOn(tasks.named("installDist"))
-    }
-
-    runAppContainer.apply {
-        dependsOn(runMongo)
-        dependsOn(buildAppImage)
-    }
-
-    val removeNetwork = remove(network).apply {
-        dependsOn(removeMongo,removeAppImage)
-    }
-    removeAppContainer.configure {
-        finalizedBy(removeNetwork)
+            service("server") {
+                image("$name:$version")
+                restart("always")
+                port(8080, 8080)
+                dependsOn(mongo)
+            }
+        }
     }
 }
--- app/src/main/kotlin/sentinel/main.kt
+++ app/src/main/kotlin/sentinel/main.kt
@@ -1,39 +1,40 @@
 package sentinel
 
-import com.mongodb.kotlin.client.coroutine.MongoClient
+//import com.mongodb.kotlin.client.coroutine.MongoClient
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.cio.*
 import io.ktor.server.engine.*
 import io.ktor.server.plugins.cors.routing.*
+import io.ktor.server.response.respondText
 import io.ktor.server.routing.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.SupervisorJob
-import kotlinx.serialization.json.Json
-import krono.SystemClock
-import lexi.ConsoleAppender
-import lexi.Logger
-import raven.AddressInfo
-import raven.LocalMemoryMailbox
-import raven.MockMailer
-import raven.MockMailerConfig
+//import kotlinx.serialization.json.Json
+//import krono.SystemClock
+//import lexi.ConsoleAppender
+//import lexi.Logger
+//import raven.AddressInfo
+//import raven.LocalMemoryMailbox
+//import raven.MockMailer
+//import raven.MockMailerConfig
 
 fun main() {
-    val scope = CoroutineScope(SupervisorJob())
-    val client = MongoClient.create("mongodb://root:pass@mongo:27017/")
-    val db = client.getDatabase("test-trial")
-    val clock = SystemClock()
-    val mailer = MockMailer(MockMailerConfig(box = LocalMemoryMailbox()))
-    val email = RegistrationEmailConfig(
-        address = AddressInfo(email = "registration@test.com", name = "Tester"),
-        subject = "Please Verify Your Email",
-        template = "Hi {{name}}, here is your token {{token}}"
-    )
-    val logger = Logger(ConsoleAppender())
-    val config = RegistrationServiceFlixConfig(scope, db, clock, mailer, logger, email)
-    val service = RegistrationServiceFlix(config)
-    val endpoint = RegistrationEndpoint("/api/v1")
-    val json = Json {}
+//    val scope = CoroutineScope(SupervisorJob())
+//    val client = MongoClient.create("mongodb://root:pass@mongo:27017/")
+//    val db = client.getDatabase("test-trial")
+//    val clock = SystemClock()
+//    val mailer = MockMailer(MockMailerConfig(box = LocalMemoryMailbox()))
+//    val email = RegistrationEmailConfig(
+//        address = AddressInfo(email = "registration@test.com", name = "Tester"),
+//        subject = "Please Verify Your Email",
+//        template = "Hi {{name}}, here is your token {{token}}"
+//    )
+//    val logger = Logger(ConsoleAppender())
+//    val config = RegistrationServiceFlixConfig(scope, db, clock, mailer, logger, email)
+//    val service = RegistrationServiceFlix(config)
+//    val endpoint = RegistrationEndpoint("/api/v1")
+//    val json = Json {}
 
     embeddedServer(CIO, port = 8080) {
         install(CORS) {
@@ -44,7 +45,10 @@ fun main() {
             allowMethod(HttpMethod.Delete)
         }
         routing {
-            installRegistration(service = service, endpoint, json)
+            get("/works") {
+                call.respondText("works")
+            }
+//            installRegistration(service = service, endpoint, json)
         }
     }.start(true)
 }
