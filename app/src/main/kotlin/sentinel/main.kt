d7ede9a912f2386891da903368def45f8a56ea9a
andylamax <andylamax@programmer.net>
2023-11-30 13:46:08 +0300

    Fixed upload progress by a mathematical estimation algorith

--- app/src/main/kotlin/sentinel/main.kt
+++ app/src/main/kotlin/sentinel/main.kt
@@ -6,9 +6,6 @@ import io.ktor.server.cio.CIO
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.plugins.cors.routing.CORS
 import io.ktor.server.routing.routing
-import java.io.File
-import sanity.installSanity
-import sentinel.info.installInfo
 
 fun main(vararg args: String) {
     val controller = SentinelAppConfiguration.parse(args.getOrNull(0)).toController()
