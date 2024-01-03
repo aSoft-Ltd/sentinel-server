3c0905f2c58594973696bb8ea91919b876e69945
andylamax <andylamax@programmer.net>
2024-01-03 10:29:38 +0300

    About to publish an sdk update for picapital

--- enterprise/authentication/core/src/commonMain/kotlin/sentinel/AuthenticationService.kt
+++ enterprise/authentication/core/src/commonMain/kotlin/sentinel/AuthenticationService.kt
@@ -1,11 +1,6 @@
 package sentinel
 
 import koncurrent.Later
-import koncurrent.later.then
-import koncurrent.later.andThen
-import koncurrent.later.andZip
-import koncurrent.later.zip
-import koncurrent.later.catch
 import sentinel.params.SendPasswordResetParams
 
 interface AuthenticationService : AuthenticationScheme {
