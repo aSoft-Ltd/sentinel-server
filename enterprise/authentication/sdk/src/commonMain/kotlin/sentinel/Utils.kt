7c3d915992f66619fea62f1e1d2cd422dd2bf048
george <george.sechu@gmail.com>
2024-08-01 13:00:59 +0300

    pimonitor application status actions

--- enterprise/authentication/sdk/src/commonMain/kotlin/sentinel/Utils.kt
+++ enterprise/authentication/sdk/src/commonMain/kotlin/sentinel/Utils.kt
@@ -3,10 +3,11 @@ package sentinel
 import io.ktor.server.application.ApplicationCall
 import io.ktor.server.application.call
 import io.ktor.server.request.header
+import io.ktor.server.routing.*
 import io.ktor.util.pipeline.PipelineContext
 import sentinel.exceptions.MissingAuthenticationException
 
-fun PipelineContext<*,ApplicationCall>.bearerToken(): String {
+fun RoutingContext.bearerToken(): String {
     val auth = call.request.header("Authorization") ?: throw MissingAuthenticationException()
     return auth.replace("Bearer ","")
 }
